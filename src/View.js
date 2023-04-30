7ca2ecf387e0e641ee19f06bb9a334b17018bd4c
Nuaritus <nuaritus@gmail.com>
2023-04-30 17:44:16 +0300

    edit_view

--- src/View.js
+++ src/View.js
@@ -10,12 +10,12 @@ class View {
 
     // Тут всё рисуем.
     console.clear();
-    console.log(this.game.track.join(""));
     console.log("\n");
     console.log(`Здоровье правого врага: ${this.game.enemy.health}`);
     console.log(`Здоровье левого врага: ${this.game.enemy2.health}\n`);
     console.log(`Твои жизни: ${this.game.hero.lives}\n`);
-    console.log(`Counter: ${this.game.count-1}`);
+    console.log(this.game.track.join(""));
+    console.log(`\nУбито врагов: ${this.game.count - 1}`);
     console.log("\x1b[1m", "\x1b[35m");
     console.log(`Игра разработана \x1b[36m${yourTeamName} 🤟🤟🤟\n`);
     console.log("\x1b[0m");
